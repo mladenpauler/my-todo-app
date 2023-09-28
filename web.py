1901cb3d2f608d14d5adf9458516c2129f27766c
mladenp <pauler1977@yahoo.com>
2023-09-28 14:19:22 -0400

    Changed gitignore

--- web.py
+++ web.py
@@ -10,10 +10,14 @@ def add_todo():
 if "my_text" not in st.session_state:
     st.session_state.new_todo = ""
 
+
 st.title("Lista za kupovinu")
 #st.subheader("This is my todo app.")
 st.write("Ukucaj dole jedan po jedan artikal i pritisni Enter.")
 st.write("Ako napravis gresku samo klikni na kockicu pored artikla i to ce ga izbrisati.")
+
+st.text_input(label="Ukucaj proizvod", placeholder="Ukucaj novi proizvod...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -23,6 +27,5 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="Ukucaj proizvod", placeholder="Ukucaj novi proizvod...",
-              on_change=add_todo, key='new_todo')
+
 
